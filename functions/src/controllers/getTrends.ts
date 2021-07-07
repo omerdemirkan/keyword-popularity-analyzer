d0c6af4b66d16601ae713d406b57eeb56b18e246
omerdemirkan <omerfarukpiano@gmail.com>
2021-07-07 10:01:26 -0700

    create src directory for client

--- functions/src/controllers/getTrends.ts
+++ functions/src/controllers/getTrends.ts
@@ -1,11 +1,12 @@
-import { fetchGoogleTrends } from "../services";
+import { fetchKeywordPopularity } from "../services";
 import { Controller } from "../types";
 
 export const getTrends: Controller = async function (req, res) {
   const keyword = req.query.keyword;
+  const weeks = +(req.query.weeks as any) || 104;
   try {
     if (typeof keyword !== "string") throw new Error("keyword query expected.");
-    const results = await fetchGoogleTrends({ keyword });
+    const results = await fetchKeywordPopularity(keyword, weeks);
     res.send(
       `Results successfully fetched!\n<pre>${JSON.stringify(
         results,
