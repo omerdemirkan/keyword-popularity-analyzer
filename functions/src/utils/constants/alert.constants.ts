6a80ba657045c846c3501bcf19388a1d689280e0
omerdemirkan <omerfarukpiano@gmail.com>
2021-12-19 10:57:08 -0800

    update minimum alert week low to 52

--- functions/src/utils/constants/alert.constants.ts
+++ functions/src/utils/constants/alert.constants.ts
@@ -5,5 +5,5 @@ export const ALERT_KEYWORDS = Object.freeze([
   "ethereum price",
 ]);
 export const GOOGLE_TRENDS_MAX_WEEKS = 104;
-export const MIN_ALERT_WEEK_LOW = 26;
+export const MIN_ALERT_WEEK_LOW = 52;
 export const WEEKS_IN_A_DECADE = 52 * 10;
